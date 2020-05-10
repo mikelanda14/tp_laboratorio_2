cbb99f3d3745c1ce1d7ea441cfb5fd344eee4f44
Licht Vather <lichtvather@gmail.com>
2020-05-09 22:41:44 -0300

    Tp2

---
+++ TP Nro 2/Entidades/Automovil.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades
+{
+    public class Automovil:Vehiculo
+    {
+        public enum ETipo { Monovolumen, Sedan }
+        private ETipo tipo;
+
+        #region Constructores
+
+        
+        /// <summary>
+        /// Por defecto, TIPO será Monovolumen
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Automovil(EMarca marca, string chasis, ConsoleColor color)
+            : base(chasis, marca, color)
+        {
+           
+        } 
+        /// <summary>
+        /// Constructor Automobil sobrecarga al por defecto y establece el Tipo
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        /// <param name="tipo"></param>
+        public Automovil(EMarca marca, string chasis, ConsoleColor color,ETipo tipo)
+            :this(marca, chasis, color)
+        {
+            if (tipo.ToString() == "Sedan")
+            {
+                this.tipo = ETipo.Sedan;
+            }
+            else { 
+            this.tipo = ETipo.Monovolumen;
+            }
+        }
+        #endregion
+
+        #region Propiedades
+
+        
+        /// <summary>
+        /// Los automoviles son medianos
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Mediano;
+            }
+        }
+        #endregion
+
+        #region Metodos
+        /// <summary>
+        /// Override Mostrar 
+        /// </summary>
+        /// <returns>Retorna string agregando  tipo y tamanio al string. utiliza mostar de la clase Base.</returns>
+
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("AUTOMOVIL");
+            sb.AppendLine(base.Mostrar());
+            sb.Append($"TAMAÑO : {this.Tamanio}");
+            sb.Append(" TIPO : " + this.tipo);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
---
+++ TP Nro 2/Entidades/Estacionamiento.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    /// <summary>
+    /// No podrá tener clases heredadas.
+    /// </summary>
+    public sealed class Estacionamiento
+    {
+        List<Vehiculo> vehiculos;
+        private int espacioDisponible;
+        public enum ETipo
+        {
+            Moto, Automovil, Camioneta, Todos
+        }
+
+        #region "Constructores"
+        /// <summary>
+        /// Constructor privado que inicializa la lista de del Atributo Vehiculos
+        /// </summary>
+        private Estacionamiento()
+        {
+            this.vehiculos = new List<Vehiculo>();
+        }
+        /// <summary>
+        /// Constructor que inicializa espacios Disponibles.
+        /// </summary>
+        /// <param name="espacioDisponible"></param>
+        public Estacionamiento(int espacioDisponible):this()
+        {
+            this.espacioDisponible = espacioDisponible;
+        }
+        #endregion
+
+        #region "Sobrecargas"
+        /// <summary>
+        /// Muestro el estacionamiento y TODOS los vehículos
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return Estacionamiento.Mostrar(this, ETipo.Todos);
+        }
+        #endregion
+
+        #region "Métodos"
+
+        /// <summary>
+        /// Expone los datos del elemento y su lista (incluidas sus herencias)
+        /// SOLO del tipo requerido
+        /// </summary>
+        /// <param name="c">Elemento a exponer</param>
+        /// <param name="ETipo">Tipos de ítems de la lista a mostrar</param>
+        /// <returns></returns>
+        public static string Mostrar(Estacionamiento c, ETipo tipo)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("Tenemos {0} lugares ocupados de un total de {1} disponibles", c.vehiculos.Count, c.espacioDisponible);
+            sb.AppendLine("");
+            foreach (Vehiculo v in c.vehiculos)
+            {
+                switch (tipo)
+                {
+                    case ETipo.Camioneta:
+                        sb.AppendLine(v.Mostrar());
+                        break;
+                    case ETipo.Moto:
+                        sb.AppendLine(v.Mostrar());
+                        break;
+                    case ETipo.Automovil:
+                        sb.AppendLine(v.Mostrar());
+                        break;
+                    default:
+                        sb.AppendLine(v.Mostrar());
+                        break;
+                }
+            }
+
+            return sb.ToString();
+        }
+        #endregion
+
+        #region "Operadores"
+        /// <summary>
+        /// Agregará un elemento a la lista
+        /// </summary>
+        /// <param name="c">Objeto donde se agregará el elemento</param>
+        /// <param name="p">Objeto a agregar</param>
+        /// <returns></returns>
+        public static Estacionamiento operator +(Estacionamiento c, Vehiculo p)
+        {
+
+                foreach (Vehiculo v in c.vehiculos)
+                {
+                   
+                    if (v == p)
+                        return c;
+                }
+
+                if (c.espacioDisponible > c.vehiculos.Count)
+                {
+                    c.vehiculos.Add(p);
+                }
+                return c;
+            
+        }
+        /// <summary>
+        /// Quitará un elemento de la lista
+        /// </summary>
+        /// <param name="c">Objeto donde se quitará el elemento</param>
+        /// <param name="p">Objeto a quitar</param>
+        /// <returns></returns>
+        public static Estacionamiento operator -(Estacionamiento c, Vehiculo p)
+        {
+            foreach (Vehiculo v in c.vehiculos)
+            {
+                if (v == p)
+                {
+                    c.vehiculos.Remove(p);
+                    break;
+                }
+            }
+
+            return c;
+        }
+        #endregion
+    }
+}
